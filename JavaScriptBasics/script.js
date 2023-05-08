308de0bcacb943006c263915aa8854b40226bd20
ShaikvSubhani <shaik4321subhani@gmail.com>
2023-05-08 18:11:15 +0530

    update

---
+++ JavaScriptBasics/script.js
@@ -0,0 +1,36 @@
+alert("hello world");
+var age=prompt("what's your age?")
+alert(age);
+document.write(age);
+
+
+function declaration
+function  function_name(param 1, param 2,param 3,.......) {
+    
+}
+
+function_name();
+
+function add(a,b)
+{
+    console.log(a+b)
+}
+
+add(2,3)
+
+var multiply=function add(a,b)
+{
+    console.log(a+b)
+}
+
+console.log(multiply)
+
+//program in javascript to check whether a number is even or odd.
+
+var a=prompt();
+if(a%2 ==0)
+{
+    console.log("even")
+}
+else 
+console.log("odd");
