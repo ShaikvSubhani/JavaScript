9cc3a0e1eacd6eb88e0ec97501cabc402e131739
ShaikvSubhani <shaik4321subhani@gmail.com>
2023-05-17 10:30:29 +0530

    update

---
+++ Arrays2/classes.js
@@ -0,0 +1,11 @@
+class Student{
+    constructor(name,aadhar,age)
+    {
+        this.name=name;
+        this.aadhar=aadhar;
+        this.age=age;
+    }
+}
+
+let s1=new Student("subhani",124542245,23);
+console.log(s1);
